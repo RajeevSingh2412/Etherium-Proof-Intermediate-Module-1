0ad7bc7b4982f69319065006f4f813030e14e265
RajeevSingh2412 <118631381+RajeevSingh2412@users.noreply.github.com>
2024-06-08 16:50:35 +0530

    Update and rename BalanceManagement.sol to SeaPortManagement.sol

---
+++ SeaPortManagement.sol
@@ -0,0 +1,58 @@
+//SPDX-License-Identifier: MIT
+pragma solidity 0.8.25;
+
+contract seaport{
+    address public owner;
+    uint public totalports=5;
+    uint public total_ships=0;
+
+    constructor(){
+        owner=msg.sender;
+    }
+
+    modifier onlyOwner(){
+        require(msg.sender==owner,"Only Owner Can Access");
+        _;
+    }
+
+    mapping(uint=>bool) public portavailability;
+    mapping(uint=>address) public shipatport;
+
+    function dockShip(address _shipaddress) public onlyOwner{
+        require(total_ships<5,"no port available");
+        for(uint i=1;i<=5;i++){
+            if(portavailability[i]==false){
+                portavailability[i]=true;
+                shipatport[i]=_shipaddress;
+                total_ships++;
+                break;
+            }
+        }
+    }
+
+    function undockShip(address _shipaddress)public onlyOwner{
+        require(total_ships>0,"no ship to remove");
+        uint portofship=1;
+        bool shipfound=false;
+
+        for(uint i=1;i<=5;i++){
+            if(shipatport[i]==_shipaddress){
+                portofship=i;
+                shipfound=true;
+            }
+        }
+
+        shipatport[portofship]=address(0);
+        portavailability[portofship]=false;
+        total_ships--;
+
+        if(!shipfound){
+            revert("Ship Not Found!");
+        }
+
+    }
+
+    function assertAvailabilty() public view{
+        assert(total_ships>0 && total_ships<=5);
+    }
+}
